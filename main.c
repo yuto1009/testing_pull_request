e25d8e7fbf99c1d3c9e8d44e1d1615fa88431b02
Kazushi Yoshida <kyoshida@c1r15s4.42tokyo.jp>
2023-12-26 17:21:19 +0900

    a,b mainas func create

--- main.c
+++ main.c
@@ -6,16 +6,22 @@ int sum_func(int a,int b)
 
 }
 
+int mainas_ab(int a,int b)
+{
+    return a-b;
+
+}
 
 int main(void)
 {
     int a = 10;
     int b = 5;
     int c = 0;
-
+    int d = 0;
     c = sum_func(a,b);
-    
-    printf("c is %d\n", c);
+    d = mainas_ab(a,b);
 
+    printf("c is %d\n", c);
+    printf("d is %d\n",d);
     return (0);
 }
