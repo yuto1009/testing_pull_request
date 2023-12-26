864f61250b482be05582c440077f43c552944c60
Kazushi Yoshida <kyoshida@c1r15s4.42tokyo.jp>
2023-12-26 17:14:44 +0900

    create a+b function

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
+int sum_func(int a,int b)
+{
+    return a+b;
+
+}
+
+
 int main(void)
 {
     int a = 10;
     int b = 5;
     int c = 0;
 
+    c = sum_func(a,b);
+    
     printf("c is %d\n", c);
 
     return (0);
